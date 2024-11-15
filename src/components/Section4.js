58bf40ff94012d0e8b6a1000d64ed90534e4c0b5
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-15 21:55:43 +0530

    change color

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -6,7 +6,7 @@ function Section4() {
 
     return (
         <div style={{
-            backgroundColor: theme === "dark" ? "#1e293b" : "white",
+            backgroundColor: theme === "dark" ? "#1e293b" : "grey",
             color: theme === "dark" ? "white" : "#1e293b"
         }}>
             <h2>Section 4</h2>
